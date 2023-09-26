ae23dfb3ce75cb490f39178878baae46761d2d90
Arianna Rizzieri <arianna.rizzieri@gmail.com>
2023-09-26 16:03:49 +0200

    added line to get input cmb

--- tutorials/tutorial_get_freq_maps.py
+++ tutorials/tutorial_get_freq_maps.py
@@ -5,7 +5,9 @@ from non_parametric_ML_compsep import *
 
 # General parameters
 NSIDE = 128
-model = 'c1d0s0'
+cmb_model = 'c1'
+fgs_model = 'd0s0'
+model = cmb_model+fgs_model
 noise = True
 noise_seed = 42
 instr_name = 'LiteBIRD'
@@ -18,3 +20,6 @@ np.random.seed(noise_seed)
 freq_maps = get_observation(instrument, model, nside=NSIDE, noise=noise)[:, 1:, :]   # keep only Q and U
 print(freq_maps.shape)
 
+# get input cmb
+input_cmb_maps = get_observation(instrument, cmb_model, nside=NSIDE, noise=False)[:, 1:, :]   # keep only Q and U
+print(input_cmb_maps.shape)
