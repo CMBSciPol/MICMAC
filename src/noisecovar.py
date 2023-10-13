8fc33de7db61043083c270558649fd8f2bdcbc6c
Arianna Rizzieri <arianna.rizzieri@gmail.com>
2023-10-13 17:27:41 +0200

    added Wd among noise covar operations

--- src/noisecovar.py
+++ src/noisecovar.py
@@ -14,54 +14,78 @@ def get_noise_covar(depth_p, nside):
     depth_p: in uK.arcmin, one value per freq channel
     nside: nside of the input maps
     """
-    invN = np.linalg.inv(np.diag((depth_p / hp.nside2resol(nside, arcmin=True))**2))
-    
+    invN = np.linalg.inv(np.diag((depth_p / hp.nside2resol(nside, arcmin=True)) ** 2))
+
     return invN
-
-
-def get_inv_BtinvNB(invN, B, jax_use=False):
-    """
-    B can be full Mixing Matrix, 
-    or just the cmb part, 
-    or just the fgs part.
-    """
-
-    if jax_use:
-        BtinvNB = jnp.einsum('fc,fh,hg->cg', B, invN, B)
-        return jnp.linalg.inv(BtinvNB)
-
-    BtinvNB = np.einsum('fc,fh,hg->cg', B, invN, B)
-    invBtinvNB = np.linalg.inv(BtinvNB)
-    
-    return invBtinvNB
 
 
 def get_BtinvN(invN, B, jax_use=False):
     """
-    B can be full Mixing Matrix, 
-    or just the cmb part, 
+    B can be full Mixing Matrix,
+    or just the cmb part,
     or just the fgs part.
     """
 
     if jax_use:
-        return jnp.einsum('fc,fh->ch', B, invN)
+        return jnp.einsum("fc,fh->ch", B, invN)
 
-    BtinvN = np.einsum('fc,fh->ch', B, invN)
-    
-    return BtinvN
+    return np.einsum("fc,fh->ch", B, invN)
+
+
+def get_BtinvNB(invN, B, jax_use=False):
+    """
+    B can be full Mixing Matrix,
+    or just the cmb part,
+    or just the fgs part.
+    """
+    BtinvN = get_BtinvN(invN, B, jax_use)
+
+    if jax_use:
+        return jnp.einsum("ch,hf->cf", BtinvN, B)
+
+    return np.einsum("ch,hf->cf", BtinvN, B)
+
+
+def get_inv_BtinvNB(invN, B, jax_use=False):
+    """
+    B can be full Mixing Matrix,
+    or just the cmb part,
+    or just the fgs part.
+    """
+    BtinvNB = get_BtinvNB(invN, B, jax_use)
+
+    if jax_use:
+        return jnp.linalg.inv(BtinvNB)
+
+    return np.linalg.inv(BtinvNB)
+
+
+def get_Wd(invN, B, d, jax_use=False):
+    """
+    invN: inverse noise covar matrix
+    B: mixing matrix
+    d: frequency maps
+    returns: W d = inv(Bt invN B) Bt invN d
+    """
+    invBtinvNB = get_inv_BtinvNB(invN, B, jax_use)
+
+    if jax_use:
+        return jnp.einsum("cf,fh,hg,fsp->csp", invBtinvNB, invN, B, d)
+
+    return np.einsum("cg,hg,hf,fsp->csp", invBtinvNB, B, invN, d)
 
 
 ## Choose if we want to keep these ones
 ## (this could be done directly in the code)
 def select_cmb_EtX(X, ncomp):
     """
-    It acts on object w ncomp as external dimension 
+    It acts on object w ncomp as external dimension
     and it selects only cmb.
     """
     # check that the ext dim is indeed ncomp
     assert X.shape[0] == ncomp
     EtX = X[0, ...]
-    
+
     return EtX
 
 
@@ -74,5 +98,5 @@ def select_cmb_EtXE(X, ncomp):
     assert X.shape[0] == ncomp
     assert X.shape[1] == ncomp
     EtXE = X[0, 0, ...]
-    
+
     return EtXE
