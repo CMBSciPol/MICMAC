d1f84f38d9eaa0d68ec5a8c03d90a24eba7a1db6
Arianna Rizzieri <arianna.rizzieri@gmail.com>
2023-10-23 13:22:56 +0200

    corrected init mixing matrix

--- src/init_mixingmatrix.py
+++ src/init_mixingmatrix.py
@@ -28,12 +28,14 @@ class InitMixingMatrix():
         # Conversion factor at frequency nu
         K_rj2K_cmb = ((np.expm1(h_over_k * nu / Tcmb)**2
                      / (np.exp(h_over_k * nu / Tcmb) * (h_over_k * nu / Tcmb)**2)))
+        
         return K_rj2K_cmb
     
 
     def K_rj2K_cmb_nu0(self, nu, nu0):
         # Conversion factor at frequency nu divided by the one at frequency nu0
         K_rj2K_cmb_nu0 = self.K_rj2K_cmb(nu) / self.K_rj2K_cmb(nu0)
+        
         return K_rj2K_cmb_nu0
     
 
@@ -64,20 +66,32 @@ class InitMixingMatrix():
         analytic_expr *= self.K_rj2K_cmb_nu0(nu, nu0)
         
         return analytic_expr
-    
+
 
     def init_params(self):
-        # implemented only the case 1st_fgs=dust(MBB), 2nd_fgs=synch(PL)
+        # implemented only the case 1st_fgs=synch(PL), 2nd_fgs=dust(MBB)
+        # TODO: genralize
         assert self.ncomp-1 == 2
         # remove the special freqs
         unknown_freqs = np.delete(self.freqs, self.pos_special_freqs)
+        known_freqs = np.array([self.freqs[self.pos_special_freqs[0]], self.freqs[self.pos_special_freqs[1]]])
+        assert list(known_freqs) == [f for f in self.freqs if f not in unknown_freqs]
         # get nu0
         nu0_dust = self.freqs[self.pos_special_freqs[0]]
         nu0_synch = self.freqs[self.pos_special_freqs[1]]
-        # define the params
-        params = np.zeros((len(unknown_freqs),self.ncomp-1))
+        # define the params w true fgs SEDs (true A entries)
+        params_ = np.zeros((len(unknown_freqs),self.ncomp-1))
         for f, val_f in enumerate(unknown_freqs):
-            params[f, 0] = self.modifiedblackbody(nu0_dust, val_f)
-            params[f, 1] = self.powerlaw(nu0_synch, val_f)
+            params_[f, 0] = self.powerlaw(nu0_synch, val_f)
+            params_[f, 1] = self.modifiedblackbody(nu0_dust, val_f)
+        # get A_f1 (SEDs of the fgs at the unknown freqs)
+        A_f1 = np.zeros((self.ncomp-1, self.ncomp-1))
+        for j, val_j in enumerate(known_freqs):
+            A_f1[j, 0] = self.powerlaw(nu0_synch, val_j)
+            A_f1[j, 1] = self.modifiedblackbody(nu0_dust, val_j)
+        inv_A_f1 = np.linalg.inv(A_f1)
+        # get params w SEDs for redefined fgs (true B entries)
+        # B = A invM
+        params = np.einsum('fc,cg->fg', params_, inv_A_f1)
 
         return params
