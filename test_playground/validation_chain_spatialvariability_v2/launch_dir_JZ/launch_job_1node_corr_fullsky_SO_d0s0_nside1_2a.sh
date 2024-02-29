b7ccbc10caab40ea0fdd6b0838859f85b4623927
Magwos <magdy.morshed.fr@gmail.com>
2024-02-28 20:24:09 -0800

    First runs multires and (of course) castrophe
    Either bug or feature, despair in any case

--- test_playground/validation_chain_spatialvariability_v2/launch_dir_JZ/launch_job_1node_corr_fullsky_SO_d0s0_nside1_2a.sh
+++ test_playground/validation_chain_spatialvariability_v2/launch_dir_JZ/launch_job_1node_corr_fullsky_SO_d0s0_nside1_2a.sh
@@ -38,7 +38,7 @@ export OMP_PLACES=cores
 
 
 
-export VER=corr_fullsky_SO_d0s0_nside1_v108_v2a
+export VER=corr_fullsky_SO_d0s0_nside1_v108_v2ab
 export additional_config_file=add_corr_fullsky_SO_d0s0_nside1_v2a.toml
 
 export SRC_PATH=/gpfswork/rech/nih/ube74zo/MICMAC/MICMAC/test_playground/validation_chain_spatialvariability_v2
