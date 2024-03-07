c79a696d2a61e9acf4b852f6bdc3ceccc664f551
Magwos <magdy.morshed.fr@gmail.com>
2024-03-06 18:38:23 -0800

    Old runs to push

---
+++ test_playground/validation_chain_spatialvariability_v3/launch_v4.sh
@@ -0,0 +1,15 @@
+# conda activate micmac_env
+
+# export path_local_MICMAC=/Users/arizzieri/APC/PhD/cmb_work/comp_sep/Non_param/MICMAC/
+export path_local_MICMAC=/Users/mag/Documents/PHD1Y/Space_Work/Pixel_non_P2D/MICMAC/
+
+export path_directory_main=$path_local_MICMAC/test_playground/validation_chain_spatialvariability_v3/
+export path_Python_script=$path_directory_main/script_v109_v3.py
+
+
+export file_ver=corr_fullsky_SO_d0s0_nside1_v109_v1bb
+export add_param_toml=add_corr_fullsky_SO_d0s0_nside1_v1bb
+
+export additional_params=$path_directory_main/additional_params/$add_param_toml.toml
+
+mpirun -n 1 python $path_Python_script $additional_params > $path_directory_main/logs/log_$file_ver.log
