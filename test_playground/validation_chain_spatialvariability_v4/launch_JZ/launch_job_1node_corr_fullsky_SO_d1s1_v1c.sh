8fdbc20366315c670c76e64433802e1d097f8c09
Magwos <magdy.morshed.fr@gmail.com>
2024-03-13 13:40:22 +0100

    New runs !

--- test_playground/validation_chain_spatialvariability_v4/launch_JZ/launch_job_1node_corr_fullsky_SO_d1s1_v1c.sh
+++ test_playground/validation_chain_spatialvariability_v4/launch_JZ/launch_job_1node_corr_fullsky_SO_d1s1_v1c.sh
@@ -36,8 +36,8 @@ export OMP_PLACES=cores
 
 
 
-export VER=corr_fullsky_SO_d1s1_nside2_v1091_v2abb
-export additional_config_file=add_corr_fullsky_SO_d1s1_nside2_v2abb.toml
+export VER=corr_fullsky_SO_d1s1_nside2_v1091_v2ab3
+export additional_config_file=add_corr_fullsky_SO_d1s1_nside2_v2ab3.toml
 
 export SRC_PATH=/gpfswork/rech/nih/ube74zo/MICMAC/MICMAC/test_playground/validation_chain_spatialvariability_v4
 
