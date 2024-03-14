5b7afdeba2544c572dd4da698b710cece3b5f2f4
Magwos <magdy.morshed.fr@gmail.com>
2024-03-14 13:49:06 +0100

    Keep computing runs until it converges

--- test_playground/validation_chain_spatialvariability_v4/launch_JZ/launch_job_1node_corr_fullsky_SO_d1s1_v1b.sh
+++ test_playground/validation_chain_spatialvariability_v4/launch_JZ/launch_job_1node_corr_fullsky_SO_d1s1_v1b.sh
@@ -36,8 +36,8 @@ export OMP_PLACES=cores
 
 
 
-export VER=corr_fullsky_SO_d1s1_nside0_v1091_v2ab
-export additional_config_file=add_corr_fullsky_SO_d1s1_nside0_v2ab.toml
+export VER=corr_fullsky_SO_d1s1_nside0_v1091_v2ab2
+export additional_config_file=add_corr_fullsky_SO_d1s1_nside0_v2ab2.toml
 
 export SRC_PATH=/gpfswork/rech/nih/ube74zo/MICMAC/MICMAC/test_playground/validation_chain_spatialvariability_v4
 
--- test_playground/validation_chain_spatialvariability_v4/launch_JZ/launch_job_1node_corr_fullsky_SO_d1s1_v1f.sh
+++ test_playground/validation_chain_spatialvariability_v4/launch_JZ/launch_job_1node_corr_fullsky_SO_d1s1_v1f.sh
@@ -36,8 +36,8 @@ export OMP_PLACES=cores
 
 
 
-export VER=corr_fullsky_SO_d1s1_nside16_v1091_v2ab10
-export additional_config_file=add_corr_fullsky_SO_d1s1_nside16_v2ab10.toml
+export VER=corr_fullsky_SO_d1s1_nside16_v1091_v2ab11
+export additional_config_file=add_corr_fullsky_SO_d1s1_nside16_v2ab11.toml
 
 export SRC_PATH=/gpfswork/rech/nih/ube74zo/MICMAC/MICMAC/test_playground/validation_chain_spatialvariability_v4
 
