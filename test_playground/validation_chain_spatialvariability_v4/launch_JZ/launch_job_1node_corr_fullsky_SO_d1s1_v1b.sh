4882b76d78050b5b3cdc1ac8a7a36fe9ba896e5b
Magwos <magdy.morshed.fr@gmail.com>
2024-03-14 17:07:54 +0100

    Typo

--- test_playground/validation_chain_spatialvariability_v4/launch_JZ/launch_job_1node_corr_fullsky_SO_d1s1_v1b.sh
+++ test_playground/validation_chain_spatialvariability_v4/launch_JZ/launch_job_1node_corr_fullsky_SO_d1s1_v1b.sh
@@ -37,7 +37,7 @@ export OMP_PLACES=cores
 
 
 export VER=corr_fullsky_SO_d1s1_nside0_v1091_v2ab3
-export additional_config_file=add_corr_fullsky_SO_d1s1_nside0_v2ab2.toml
+export additional_config_file=add_corr_fullsky_SO_d1s1_nside0_v2ab3.toml
 
 export SRC_PATH=/gpfswork/rech/nih/ube74zo/MICMAC/MICMAC/test_playground/validation_chain_spatialvariability_v4
 
