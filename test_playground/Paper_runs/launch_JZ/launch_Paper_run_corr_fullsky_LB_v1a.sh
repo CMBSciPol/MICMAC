91f766419b3f71116e09ae201bd0efde6d822ad6
Magwos <magdy.morshed.fr@gmail.com>
2024-03-17 22:00:13 +0100

    Attempt at relaunching some runs

--- test_playground/Paper_runs/launch_JZ/launch_Paper_run_corr_fullsky_LB_v1a.sh
+++ test_playground/Paper_runs/launch_JZ/launch_Paper_run_corr_fullsky_LB_v1a.sh
@@ -36,7 +36,7 @@ export OMP_PLACES=cores
 
 
 
-export VER=corr_cutsky_LB_d0s0_nside0_v1091_v1a8
+export VER=corr_cutsky_LB_d0s0_nside0_v1091_v1a9
 export additional_config_file=add_corr_cutsky_LB_d0s0_nside0_v1a.toml
 
 export SRC_PATH=/gpfswork/rech/nih/ube74zo/MICMAC/MICMAC/test_playground/Paper_runs
