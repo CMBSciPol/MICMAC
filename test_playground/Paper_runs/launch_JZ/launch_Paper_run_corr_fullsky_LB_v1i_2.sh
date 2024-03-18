9f3b9fb7473dd3d9dc980261608bca634fead48b
Magwos <magdy.morshed.fr@gmail.com>
2024-03-18 07:24:05 +0100

    Continuation Paper Runs !

--- test_playground/Paper_runs/launch_JZ/launch_Paper_run_corr_fullsky_LB_v1i_2.sh
+++ test_playground/Paper_runs/launch_JZ/launch_Paper_run_corr_fullsky_LB_v1i_2.sh
@@ -37,7 +37,7 @@ export OMP_PLACES=cores
 
 
 
-export VER=file_ver = 'corr_cutsky_LB_d0s0_nside0_v1091_r0_v1i'
+export VER=file_ver = 'corr_cutsky_LB_d0s0_nside0_v1091_r0_v1i2'
 export additional_config_file=add_corr_cutsky_LB_d0s0_nside0_v1i.toml
 
 export SRC_PATH=/gpfswork/rech/nih/ube74zo/MICMAC/MICMAC/test_playground/Paper_runs
