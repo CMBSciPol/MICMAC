19010a7f89211e571ef4794d1035322e30e0f41c
Magwos <magdy.morshed.fr@gmail.com>
2024-03-19 15:40:21 +0100

    Continuation promising run

--- test_playground/Paper_runs/launch_JZ/launch_Paper_run_corr_fullsky_LB_v2a.sh
+++ test_playground/Paper_runs/launch_JZ/launch_Paper_run_corr_fullsky_LB_v2a.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=corr_Paper_run_LB_v1a
+#SBATCH --job-name=corr_Paper_run_LB_v2a
 #SBATCH --account=nih@cpu          # use CPU allocation
 #SBATCH --qos=qos_cpu-dev          # dev qos (10 jobs, 2h max.)
 #SBATCH --mail-user=magdy.morshed.fr@gmail.com
@@ -36,7 +36,7 @@ export OMP_PLACES=cores
 
 
 
-export VER=corr_cutsky_LB_d0s0_nside0_v1091_v2a
+export VER=corr_cutsky_LB_d0s0_nside0_v1091_v2a2
 export additional_config_file=add_corr_cutsky_LB_d0s0_nside0_v2a.toml
 
 export SRC_PATH=/gpfswork/rech/nih/ube74zo/MICMAC/MICMAC/test_playground/Paper_runs
