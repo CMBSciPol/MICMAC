419f2314c78a5e45177f45edeaa43667b52b274f
Magwos <magdy.morshed.fr@gmail.com>
2024-03-24 01:36:16 +0100

    New launches

--- test_playground/Validation_Classical_Gibbs/launch_v5.sh
+++ test_playground/Validation_Classical_Gibbs/launch_v5.sh
@@ -12,7 +12,7 @@ export path_Python_script=$path_directory_main/script_v109_v4b.py
 # export file_ver=class_cutsky_1freq_nofg_v1a5
 # export add_param_toml=add_class_cutsky_f1freq_nofg_v1a
 
-export file_ver=class_cutsky_1freq_nofg_v1cc
+export file_ver=class_cutsky_1freq_nofg_v1cc2
 export add_param_toml=add_class_cutsky_f1freq_nofg_v1c
 
 # export file_ver=class_cutsky_1freq_nofg_v1d
