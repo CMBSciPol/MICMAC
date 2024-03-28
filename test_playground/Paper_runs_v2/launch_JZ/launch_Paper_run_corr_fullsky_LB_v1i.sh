eb7a0292f1bcd7e63711cd785b0ae8a6a3d0895b
Magwos <magdy.morshed.fr@gmail.com>
2024-03-28 14:21:09 +0100

    Relaunch after typo

--- test_playground/Paper_runs_v2/launch_JZ/launch_Paper_run_corr_fullsky_LB_v1i.sh
+++ test_playground/Paper_runs_v2/launch_JZ/launch_Paper_run_corr_fullsky_LB_v1i.sh
@@ -36,7 +36,7 @@ export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 export OMP_PLACES=cores
 
 
-export VER=corr_cutsky_LB_d0s0_r0_v1i2
+export VER=corr_cutsky_LB_d0s0_r0_v1i
 export additional_config_file=add_corr_cutsky_LB_d0s0_v1i.toml
 
 export SRC_PATH=/gpfswork/rech/nih/ube74zo/MICMAC/MICMAC/test_playground/Paper_runs_v2
