8c31da80b35998bdff25b7958faf1b907aca7c4e
Magwos <magdy.morshed.fr@gmail.com>
2024-03-30 00:43:40 +0100

    Continuations some runs, to check everything

--- test_playground/Paper_runs_v2/launch_JZ/launch_Paper_run_corr_fullsky_SO_v1f.sh
+++ test_playground/Paper_runs_v2/launch_JZ/launch_Paper_run_corr_fullsky_SO_v1f.sh
@@ -35,7 +35,6 @@ export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 # OpenMP binding
 export OMP_PLACES=cores
 
-
 export VER=corr_cutsky_SO_d7s0_nside2_v1091b_v1a
 export additional_config_file=add_corr_cutsky_SO_d7s0_nside2_v1a.toml
 
