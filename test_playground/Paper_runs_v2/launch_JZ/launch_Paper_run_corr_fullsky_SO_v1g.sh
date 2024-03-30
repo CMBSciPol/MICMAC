7d2b3bc7ed5672eb3668a4f7882cf3aca152f107
Magwos <magdy.morshed.fr@gmail.com>
2024-03-30 18:15:34 +0100

    Relaunch

--- test_playground/Paper_runs_v2/launch_JZ/launch_Paper_run_corr_fullsky_SO_v1g.sh
+++ test_playground/Paper_runs_v2/launch_JZ/launch_Paper_run_corr_fullsky_SO_v1g.sh
@@ -35,7 +35,7 @@ export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 # OpenMP binding
 export OMP_PLACES=cores
 
-export VER=corr_cutsky_SO_d7s0_nside4_v1091b_v1a2
+export VER=corr_cutsky_SO_d7s0_nside4_v1091b_v1a3
 export additional_config_file=add_corr_cutsky_SO_d7s0_nside4_v1a.toml
 
 export SRC_PATH=/gpfswork/rech/nih/ube74zo/MICMAC/MICMAC/test_playground/Paper_runs_v2
