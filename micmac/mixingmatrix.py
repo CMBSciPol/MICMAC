80862c1e1e12ca0612c68690b0c621a45f8a44fa
Alexandre Boucaud <aboucaud@apc.in2p3.fr>
2024-04-06 19:58:21 +0200

    Add license text for each file

--- micmac/mixingmatrix.py
+++ micmac/mixingmatrix.py
@@ -1,3 +1,19 @@
+# This file is part of MICMAC.
+# Copyright (C) 2024 CNRS / SciPol developers
+#
+# MICMAC is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# MICMAC is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with MICMAC. If not, see <https://www.gnu.org/licenses/>.
+
 import copy
 
 import chex as chx
