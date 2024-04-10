7e759d5e1beaf0b1ee1b40cfd1ca63c0f225ca67
Magwos <magdy.morshed.fr@gmail.com>
2024-04-10 16:21:14 +0200

    Pushing further

--- test_playground/Paper_runs_v3/launch_JZ/launch_Paper_run_corr_v2e.sh
+++ test_playground/Paper_runs_v3/launch_JZ/launch_Paper_run_corr_v2e.sh
@@ -39,7 +39,7 @@ export OMP_PLACES=cores
 # export VER=corr_cutsky_SO_d0s0_nside0_v1091c_r0_v1a5
 # export additional_config_file=add_corr_cutsky_SO_d0s0_nside0_v1a.toml
 
-export VER=corr_cutsky_SO_d0s0_nside0_v1091c_r0_inhom_v1a8
+export VER=corr_cutsky_SO_d0s0_nside0_v1091c_r0_inhom_v1a9
 export additional_config_file=add_corr_cutsky_SO_d0s0_nside0_inhom_v1a.toml
 
 
