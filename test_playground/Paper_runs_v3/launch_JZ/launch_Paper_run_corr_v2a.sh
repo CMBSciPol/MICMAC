98e64326dc948dae98a01e8be8cd2607fc6ce263
Magwos <magdy.morshed.fr@gmail.com>
2024-04-12 22:10:07 +0200

    Pushing futher

--- test_playground/Paper_runs_v3/launch_JZ/launch_Paper_run_corr_v2a.sh
+++ test_playground/Paper_runs_v3/launch_JZ/launch_Paper_run_corr_v2a.sh
@@ -45,7 +45,7 @@ export OMP_PLACES=cores
 # export VER=corr_fullsky_LB_d0s0_r0_v1be6
 # export additional_config_file=add_corr_fullky_LB_d0s0_v1be.toml
 
-export VER=corr_fullsky_LB_d0s0_r0_v1beb6
+export VER=corr_fullsky_LB_d0s0_r0_v1beb7
 export additional_config_file=add_corr_fullky_LB_d0s0_v1beb.toml
 
 
--- test_playground/Paper_runs_v3/launch_JZ/launch_Paper_run_corr_v2j.sh
+++ test_playground/Paper_runs_v3/launch_JZ/launch_Paper_run_corr_v2j.sh
@@ -45,7 +45,7 @@ export OMP_PLACES=cores
 # export VER=corr_fullsky_LB_d0s0_r0_v1bg6
 # export additional_config_file=add_corr_fullky_LB_d0s0_v1bg.toml
 
-export VER=corr_fullsky_LB_d0s0_r0_v1bgb6
+export VER=corr_fullsky_LB_d0s0_r0_v1bgb7
 export additional_config_file=add_corr_fullky_LB_d0s0_v1bgb.toml
 
 export SRC_PATH=/gpfswork/rech/nih/ube74zo/MICMAC/MICMAC/test_playground/Paper_runs_v3
