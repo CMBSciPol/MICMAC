3a51d443303540861ea073694678438b61829d1a
Magwos <magdy.morshed.fr@gmail.com>
2024-04-14 16:14:17 +0200

    Pushing further other tests

--- test_playground/Paper_runs_v3/launch_JZ/launch_Paper_run_corr_v2a.sh
+++ test_playground/Paper_runs_v3/launch_JZ/launch_Paper_run_corr_v2a.sh
@@ -48,7 +48,7 @@ export OMP_PLACES=cores
 # export VER=corr_fullsky_LB_d0s0_r0_v1beb7
 # export additional_config_file=add_corr_fullky_LB_d0s0_v1beb.toml
 
-export VER=corr_fullsky_LB_d0s0_r0_v1bec3
+export VER=corr_fullsky_LB_d0s0_r0_v1bec4
 export additional_config_file=add_corr_fullky_LB_d0s0_v1bec.toml
 
 
