a628e7375dd115c7687ff481a51c4d8868056be7
Magwos <magdy.morshed.fr@gmail.com>
2024-04-16 08:42:01 +0200

    Pushing further git push

--- test_playground/Paper_runs_v3/launch_JZ/launch_Paper_run_corr_v2a.sh
+++ test_playground/Paper_runs_v3/launch_JZ/launch_Paper_run_corr_v2a.sh
@@ -48,7 +48,7 @@ export OMP_PLACES=cores
 # export VER=corr_fullsky_LB_d0s0_r0_v1beb7
 # export additional_config_file=add_corr_fullky_LB_d0s0_v1beb.toml
 
-export VER=corr_fullsky_LB_d0s0_r0_v1bec12
+export VER=corr_fullsky_LB_d0s0_r0_v1bec13
 export additional_config_file=add_corr_fullky_LB_d0s0_v1bec.toml
 
 
