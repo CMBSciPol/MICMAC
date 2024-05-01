1de2d0bd1e5da40fbd68cce992cba98f073150a9
Magwos <magdy.morshed.fr@gmail.com>
2024-05-01 17:33:25 +0200

    Testing full sky futher

--- test_playground/Paper_runs_v3/launch_JZ/launch_Paper_run_corr_v2ad.sh
+++ test_playground/Paper_runs_v3/launch_JZ/launch_Paper_run_corr_v2ad.sh
@@ -38,7 +38,7 @@ export OMP_PLACES=cores
 # export VER=corr_fullsky_LB_d0s0_nside0_v1091c_r2_v1a
 # export additional_config_file=add_corr_fullsky_LB_d0s0_nside0_v2a.toml
 
-export VER=corr_fullsky_SO_d0s0_r0_v1eb14
+export VER=corr_fullsky_SO_d0s0_r0_v1eb15
 export additional_config_file=add_corr_fullky_SO_d0s0_v1eb.toml
 
 export SRC_PATH=/gpfswork/rech/nih/ube74zo/MICMAC/MICMAC/test_playground/Paper_runs_v3
