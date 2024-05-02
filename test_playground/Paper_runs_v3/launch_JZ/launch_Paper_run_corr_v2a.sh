7d86966fe43f8437f53866d4c1af24f9b54d3fc7
Magwos <magdy.morshed.fr@gmail.com>
2024-05-02 22:04:50 +0200

    🚧 Still validating full sky for last paper plots !

--- test_playground/Paper_runs_v3/launch_JZ/launch_Paper_run_corr_v2a.sh
+++ test_playground/Paper_runs_v3/launch_JZ/launch_Paper_run_corr_v2a.sh
@@ -53,7 +53,7 @@ export OMP_PLACES=cores
 
 # srun python $SRC_PATH/script_v109_v5b.py $additional_config_file  1> $SRC_PATH/logs/log_$VER.log 2> $SRC_PATH/errs/err_$VER.log
 
-export VER=corr_cutsky_d7s1customNonparam_SO_nside0_v1091c_r0_v1a12
+export VER=corr_cutsky_d7s1customNonparam_SO_nside0_v1091c_r0_v1a13
 export additional_config_file=add_corr_cutsky_SO_d7s1customNonparam_nside0_v1a.toml
 
 export SRC_PATH=/gpfswork/rech/nih/ube74zo/MICMAC/MICMAC/test_playground/Paper_runs_v3
