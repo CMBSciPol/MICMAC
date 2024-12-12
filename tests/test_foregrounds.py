dd678f798e8b80863489d3fbbde4200f4ba7cf3e
Magwos <magdy.morshed.fr@gmail.com>
2024-12-12 17:58:02 +0100

    First base for pytests

---
+++ tests/test_foregrounds.py
@@ -0,0 +1,24 @@
+# TODO: Complete, WORK IN PROGRESS !!!
+
+## Test that init_mixing_matrix is compatible with FGBuster matrix if built explicitely
+
+## Tests about spv -> maybe check tutorials Arianna
+
+## Check fail custom models ?
+
+## Check templates construction/basis functions
+
+
+
+# Implement following test 
+
+# print("-------------Test get_all_templates", flush=True)
+# print("-1th test", MICMAC_obj.values_b.shape, MICMAC_obj.values_b)
+# print("0th test", MICMAC_obj.size_patches)
+# print("First test", MICMAC_obj.sum_size_patches_indexed_freq_comp)
+# all_templates = MICMAC_obj.get_all_templates()
+# print("Dims: ", all_templates.shape, flush=True)
+# for j in range(all_templates.shape[1]):
+#     for i in range(all_templates.shape[0]):
+#         print(all_templates[i, j].min(), all_templates[i, j].max(), all_templates[i, j], flush=True)
+# print("-------------End test")
---
+++ tests/test_likelihood.py
@@ -0,0 +1,5 @@
+# TODO: Complete, WORK IN PROGRESS !!!
+
+## Here more test on sampling functions, maybe to compare to theoretical if no spv
+
+## If spv maybe a bit harder for comparison?
